877ce9be065adf2e2d60f556c8c138bd8d111b4f
Akkawut Yonsuk (Best) <akkawut@readyplanet.com>
2023-12-17 23:25:11 +0700

    feat: remove outline width

--- example-quiz/src/components/AddName.tsx
+++ example-quiz/src/components/AddName.tsx
@@ -1,36 +1,45 @@
-import { View, SafeAreaView, TextInput, StyleSheet, Pressable, Text } from "react-native";
+import {
+  View,
+  SafeAreaView,
+  TextInput,
+  StyleSheet,
+  Pressable,
+  Text,
+} from "react-native";
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { setCurrentUser } from '../store/leaderBoardSlice';
-import { router } from 'expo-router';
-import { setQuizState } from '../store/quizSlice';
+import { setCurrentUser } from "../store/leaderBoardSlice";
+import { router } from "expo-router";
+import { setQuizState } from "../store/quizSlice";
 
 export default function AddName() {
+  const [name, setName] = useState("" as string);
+  const dispatch = useDispatch();
 
-	const [name, setName] = useState('' as string)
-	const dispatch = useDispatch()
+  const handleNameChange = (text: string) => {
+    setName(text);
+  };
 
-	const handleNameChange = (text: string) => {
-		setName(text)
-	}
-
-	const onSubmit = () => {
-		dispatch(setCurrentUser(name))
-		dispatch(setQuizState('start'))
-		if (name !== '') {
-			router.push('/quiz')
-		}
-	}
+  const onSubmit = () => {
+    dispatch(setCurrentUser(name));
+    dispatch(setQuizState("start"));
+    if (name !== "") {
+      router.push("/quiz");
+    }
+  };
 
   return (
     <SafeAreaView>
       <View>
-				<TextInput style={styles.input} placeholder="Add Name" onChangeText={handleNameChange} />
-				<Pressable style={styles.button} onPress={onSubmit}>
-					<Text style={{ color: '#fff' }}>Start Quiz</Text>
-				</Pressable>
-			</View>
-			
+        <TextInput
+          style={styles.input}
+          placeholder="Add Name"
+          onChangeText={handleNameChange}
+        />
+        <Pressable style={styles.button} onPress={onSubmit}>
+          <Text style={{ color: "#fff" }}>Start Quiz</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 }
@@ -38,20 +47,19 @@ export default function AddName() {
 const styles = StyleSheet.create({
   input: {
     height: 40,
-		margin: 12,
-		textAlign: 'center',
-		borderBottomColor: '#f4511e',
+    margin: 12,
+    textAlign: "center",
+    borderBottomColor: "#f4511e",
     borderBottomWidth: 2,
-		padding: 10,
-		fontWeight: 'bold',
-		outlineWidth: 0,
-		placeholderTextColor: 'gray',
-	},
-	button: {
-		alignItems: 'center',
-		backgroundColor: '#f4511e',
-		padding: 10,
-		margin: 12,
-		borderRadius: 10,
-	}
+    padding: 10,
+    fontWeight: "bold",
+    placeholderTextColor: "gray",
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#f4511e",
+    padding: 10,
+    margin: 12,
+    borderRadius: 10,
+  },
 });
